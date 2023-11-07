b04baa2e9792c6ba30316a87237c0eadb7ccb5f6
oktaykara <oktaykara14@gmail.com>
2023-11-07 11:41:26 +0300

    Java 20 Updated Cicleci config added

--- src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -1,13 +1,11 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 public class IndexController {
-
     public String index(){
-
         return "index";
     }
 
-    public String oupsHandler(){
-        return "notimplemented";
+    public void oopsHandler(){
+        throw new ValueNotFoundException();
     }
 }
